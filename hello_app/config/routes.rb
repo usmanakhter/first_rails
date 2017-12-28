4fee1b83de46d20218ea61d03d1f37ebcd841d87
Usman Akhter <usmanakhter1@gmail.com>
2017-12-28 01:42:57 -0600

    switch route to #hello instead of #goodbye

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,4 +1,4 @@
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-	root 'application#goodbye'
+	root 'application#hello'
 end
